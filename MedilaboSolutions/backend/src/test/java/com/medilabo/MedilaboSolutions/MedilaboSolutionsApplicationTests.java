630c0026d47c7d62a5779da0dacca02e935675c6
MichelleMair <michelle.rk.mairet@gmail.com>
2024-12-04 13:59:24 +0100

    Generate backend project with spring initialzr

---
+++ MedilaboSolutions/backend/src/test/java/com/medilabo/MedilaboSolutions/MedilaboSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.MedilaboSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
