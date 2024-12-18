4c88c9959b4df89d278d671b7bf0108e15bae29f
MichelleMair <michelle.rk.mairet@gmail.com>
2024-12-18 13:35:47 +0100

    Implements security config for ms gateway

---
+++ MedilaboSolutions/ms-gateway/src/main/java/com/medilabo/gateway/configurations/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.medilabo.gateway.configurations;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		http.csrf(csrf -> csrf.disable()) //disable csrf for local tests
+			.cors(cors -> cors.configurationSource(corsConfigurationSource())) //Adding cors
+			.authorizeExchange(exchange -> exchange
+					.pathMatchers("/api/patients/**").authenticated() // Secured routes
+					.pathMatchers("/actuator/**").permitAll() //Authorize actuator for monitoring
+					.anyExchange().permitAll()) //all others requests are public
+			.httpBasic(withDefaults());
+		return http.build();
+	}
+	
+	@Bean
+	public MapReactiveUserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+		UserDetails user = User.withUsername("user")
+					.password(passwordEncoder.encode("password"))
+					.roles("USER")
+					.build();
+		return new MapReactiveUserDetailsService(user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
