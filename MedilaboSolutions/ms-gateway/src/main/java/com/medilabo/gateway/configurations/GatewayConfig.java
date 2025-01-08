b61fcf4857f43e2fbb5c758fef4694c5e3eb98a0
MichelleMair <michelle.rk.mairet@gmail.com>
2025-01-08 17:31:03 +0100

    Gateway configuration setup

---
+++ MedilaboSolutions/ms-gateway/src/main/java/com/medilabo/gateway/configurations/GatewayConfig.java
@@ -0,0 +1,29 @@
+package com.medilabo.gateway.configurations;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				// Route for auth service
+				.route("auth-service", r -> r.path("/api/auth/**")
+						.filters(f -> f.rewritePath("/api/auth/(?<remaining>.*)", "/${remaining}"))
+						.uri("http://localhost:8080"))
+				//Route for patient service 
+				.route("patient-service", r -> r.path("/api/patients/**")
+						.filters(f -> f.rewritePath("/api/patients/(?<remaining>.*)", "/${remaining}"))
+						.uri("http://localhost:8080"))
+				// Route for frontend
+				.route("frontend-service", r -> r.path("/ms-frontend/**")
+						.filters(f -> f.rewritePath("/ms-frontend/(?<remaining>.*)", "/${remaining}"))
+						.uri("file:///./target/classes/static"))
+				.build();
+				
+	}
+}
