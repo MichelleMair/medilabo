d82da7eb8056b4554b1fb5a568e2696a002f5d12
MichelleMair <michelle.rk.mairet@gmail.com>
2025-01-15 22:04:40 +0100

    First commit for ms notes setup

---
+++ MedilaboSolutions/ms-notes/src/main/java/com/medilabo/notes/MsNotesApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotesApplication.class, args);
+	}
+
+}
