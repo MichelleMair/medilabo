a84735edcadb8524439657684c03577c36cf9dfb
MichelleMair <michelle.rk.mairet@gmail.com>
2025-01-29 21:21:50 +0100

    First commit for diabetes risk assessment ms setup

---
+++ MedilaboSolutions/ms-diabetes-risk/src/main/java/com/medilabo/diabetes/risk/MsDiabetesRiskApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetes.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiabetesRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiabetesRiskApplication.class, args);
+	}
+
+}
---
+++ MedilaboSolutions/ms-diabetes-risk/src/test/java/com/medilabo/diabetes/risk/MsDiabetesRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diabetes.risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiabetesRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
