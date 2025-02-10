8d83eb7b3821f07dc3e2b8c6a68cd620a9387e27
MichelleMair <michelle.rk.mairet@gmail.com>
2025-02-10 11:50:38 +0100

    Back to before docker compose issues and cleaning code

--- MedilaboSolutions/ms-diabetes-risk/src/main/java/com/medilabo/diabetes/risk/client/PatientClient.java
+++ MedilaboSolutions/ms-diabetes-risk/src/main/java/com/medilabo/diabetes/risk/client/PatientClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.medilabo.diabetes.risk.model.Patient;
 
-@FeignClient(name= "ms-backend-patient", url="http://ms-backend-patient:8080")
+@FeignClient(name= "ms-backend-patient", url="http://localhost:8080")
 public interface PatientClient {
 	@GetMapping("/api/patients/{id}")
 	Patient getPatientById(@PathVariable("id") String id);
